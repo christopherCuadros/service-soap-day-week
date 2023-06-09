ec4afa675b5dddf0ed64466c17bef7a584be66b5
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-06-08 21:16:52 -0500

    estructura de proyecto

---
+++ service-soap-dayWeek/src/main/java/ejercicio/service/soap/servicesoapdayWeek/ServiceSoapDayWeekApplication.java
@@ -0,0 +1,13 @@
+package ejercicio.service.soap.servicesoapdayWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSoapDayWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSoapDayWeekApplication.class, args);
+	}
+
+}
---
+++ service-soap-dayWeek/src/test/java/ejercicio/service/soap/servicesoapdayWeek/ServiceSoapDayWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio.service.soap.servicesoapdayWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSoapDayWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
